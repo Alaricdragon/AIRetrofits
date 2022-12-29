66b1fb27d19db8f09aa97210f5455837d64461ce
Alaricdragon <alaric.meservy@gmail.com>
2022-12-29 13:50:20 -0700

    display for robot power

--- jars/src/data/scripts/AIRetrofit_RobotDescriptions.java
+++ jars/src/data/scripts/AIRetrofit_RobotDescriptions.java
@@ -3,14 +3,19 @@ package data.scripts;
 import com.fs.starfarer.api.Global;
 import com.fs.starfarer.api.campaign.CargoStackAPI;
 import com.fs.starfarer.api.campaign.listeners.CommodityTooltipModifier;
+import com.fs.starfarer.api.impl.PlayerFleetPersonnelTracker;
 import com.fs.starfarer.api.ui.TooltipMakerAPI;
+import com.fs.starfarer.api.util.Misc;
 import org.apache.log4j.Logger;
 
-public class AIRetrofit_RobotDescriptions implements CommodityTooltipModifier {
+import java.awt.*;
 
+public class AIRetrofit_RobotDescriptions implements CommodityTooltipModifier {
+    public static CommodityTooltipModifier getCommodityTooltipModifier(){
+        return (CommodityTooltipModifier) new AIRetrofit_RobotDescriptions();
+    }
     @Override
     public void addSectionAfterPrice(TooltipMakerAPI info, float width, boolean expanded, CargoStackAPI stack) {
-        loging("HERE getting data for a thing from a thing named: " + stack);
         String name = stack.getCommodityId();
         switch (name){
             case "AIretrofit_WorkerDrone":
@@ -24,14 +29,32 @@ public class AIRetrofit_RobotDescriptions implements CommodityTooltipModifier {
                 break;
         }
     }
+    AIRetrofit_Robots[] robots = {
+            (AIRetrofit_Robots) crewReplacer_Main.getJob("salvage_crew").getCrew("AIretrofit_WorkerDrone"),
+            (AIRetrofit_Robots) crewReplacer_Main.getJob("survey_crew").getCrew("AIretrofit_SurveyDrone"),
+            (AIRetrofit_Robots) crewReplacer_Main.getJob("raiding_marines").getCrew("AIretrofit_CombatDrone"),
+    };
     private void salvageRobot(TooltipMakerAPI info, float width, boolean expanded, CargoStackAPI stack){
-        info.addPara("AAAAAAAAAAAAAAAAAAA",5);
+        displayCrewPower(robots[0],info,width,expanded,stack);
     }
     private void combatRobot(TooltipMakerAPI info, float width, boolean expanded, CargoStackAPI stack){
-        info.addPara("cool",5);
+        displayCrewPower(robots[2],info,width,expanded,stack);
     }
     private void surveyRobot(TooltipMakerAPI info, float width, boolean expanded, CargoStackAPI stack){
-        info.addPara("POWER",5);
+        displayCrewPower(robots[1],info,width,expanded,stack);
+    }
+    private static String robotDescription = "this robot effectiveness is being multiplied by %s for having %s in cargo, for a total power of %s per robot";
+    private void displayCrewPower(AIRetrofit_Robots robot, TooltipMakerAPI info, float width, boolean expanded, CargoStackAPI stack){
+        Color highlight = Misc.getHighlightColor();
+        String[] exstra = {
+                "" + robot.getCorePower(Global.getSector().getPlayerFleet()),
+                robot.getMaxCore(Global.getSector().getPlayerFleet()),
+                "" + robot.getCrewPower(Global.getSector().getPlayerFleet()),
+        };
+        info.addPara(robotDescription, 5, highlight, exstra);
+        if(expanded){
+            /*this will hold the crew power of every job for this crew.*/
+        }
     }
     private void loging(String output){
         final Logger LOG = Global.getLogger(this.getClass());
--- jars/src/data/scripts/AIRetrofit_Robots.java
+++ jars/src/data/scripts/AIRetrofit_Robots.java
@@ -9,7 +9,7 @@ import com.fs.starfarer.api.campaign.CampaignFleetAPI;
         static float betaBonus = Global.getSettings().getFloat("AIRetrofits_BetaMulti");
         static float alphaBonus = Global.getSettings().getFloat("AIRetrofits_AlphaMulti");
         static float omegaBonus = Global.getSettings().getFloat("AIRetrofits_OmegaMulti");
-        static String[] AICores = {
+        final private static String[] AICores = {
                 "AIretrofit_SubCommandNode",
                 "gamma_core",
                 "beta_core",
@@ -18,6 +18,23 @@ import com.fs.starfarer.api.campaign.CampaignFleetAPI;
         };
         @Override
         public float getCrewPowerInFleet(CampaignFleetAPI fleet) {
+            return getCrewInFleet(fleet) * getCrewPower(fleet);//super.getCrewPowerInFleet(fleet);
+        }
+        public String getMaxCore(CampaignFleetAPI fleet){
+            if(fleet.getCargo().getCommodityQuantity(AICores[4]) > 0){
+                return Global.getSector().getEconomy().getCommoditySpec(AICores[4]).getName();
+            }else if(fleet.getCargo().getCommodityQuantity(AICores[3]) > 0){
+                return Global.getSector().getEconomy().getCommoditySpec(AICores[3]).getName();
+            }else if(fleet.getCargo().getCommodityQuantity(AICores[2]) > 0){
+                return Global.getSector().getEconomy().getCommoditySpec(AICores[2]).getName();
+            }else if(fleet.getCargo().getCommodityQuantity(AICores[1]) > 0){
+                return Global.getSector().getEconomy().getCommoditySpec(AICores[1]).getName();
+            }else if(fleet.getCargo().getCommodityQuantity(AICores[0]) > 0){
+                return Global.getSector().getEconomy().getCommoditySpec(AICores[0]).getName();
+            }
+            return "no AI-Core or SubCommandNode's";
+        }
+        public float getCorePower(CampaignFleetAPI fleet){
             float multi = baseBonus;
             if(fleet.getCargo().getCommodityQuantity(AICores[4]) > 0){
                 multi = omegaBonus;
@@ -30,6 +47,10 @@ import com.fs.starfarer.api.campaign.CampaignFleetAPI;
             }else if(fleet.getCargo().getCommodityQuantity(AICores[0]) > 0){
                 multi = commandBonus;
             }
-            return getCrewInFleet(fleet) * crewPower * multi;//super.getCrewPowerInFleet(fleet);
+            return multi;
+        }
+        public float getCrewPower(CampaignFleetAPI fleet){
+            float multi = getCorePower(fleet);
+            return crewPower * multi;
         }
     }
