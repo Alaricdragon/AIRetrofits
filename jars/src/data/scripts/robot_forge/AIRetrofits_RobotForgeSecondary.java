bd69996a18aaef52304fe2eb3021755175cfd46a
Alaricdragon <alaric.meservy@gmail.com>
2023-05-26 12:35:51 -0700

    V0.7.21

--- jars/src/data/scripts/robot_forge/AIRetrofits_RobotForgeSecondary.java
+++ jars/src/data/scripts/robot_forge/AIRetrofits_RobotForgeSecondary.java
@@ -7,6 +7,7 @@ import com.fs.starfarer.api.campaign.CargoAPI;
 import com.fs.starfarer.api.campaign.SpecialItemData;
 import com.fs.starfarer.api.fleet.FleetMemberAPI;
 import com.fs.starfarer.api.impl.campaign.ids.Items;
+import data.scripts.AIRetrofit_Log;
 import data.scripts.startupData.AIRetrofits_Constants;
 
 import java.util.List;
@@ -17,22 +18,14 @@ public class AIRetrofits_RobotForgeSecondary {
     static float ForgePowerMulti = AIRetrofits_Constants.RobotForge_ForgePowerMulti;//Global.getSettings().getFloat("AIRetrofits_RobotForgePerCrewMulti");
 
     public static float iCalculateBonus(CampaignFleetAPI fleet) {
-            //oldHERE. mush to do
-            //float iCorrupted = fleet.getCargo().getQuantity(CargoAPI.CargoItemType.SPECIAL, new SpecialItemData(Items.CORRUPTED_NANOFORGE, null));
-            //float iPristine = fleet.getCargo().getQuantity(CargoAPI.CargoItemType.SPECIAL, new SpecialItemData(Items.PRISTINE_NANOFORGE, null));
-            //float iSalvageCoomer = 0f;
-            List<FleetMemberAPI> playerFleetList = Global.getSector().getPlayerFleet().getFleetData().getMembersListCopy();
-            //int iShipSize = playerFleetList.size();
-            //iSalvageCoomer = getFleetsForgeModules(fleet);
-            //float iBonus = iSalvageCoomer;
-            //float temp = Math.min(iPristine,iSalvageCoomer);
-            //iBonus += temp * PristineMetalMultiplier;
-            //iSalvageCoomer -= temp;
-            //temp = Math.min(iCorrupted,iSalvageCoomer);
-            //iBonus += temp * CorruptedMetalMultiplier;
+        try {
             float iBonus = getFleetsForgeModules(fleet);
-            return iBonus + (Math.min(iBonus,getFleetsNanoforgePower(fleet)))/*+1*/;
+            return iBonus + (Math.min(iBonus, getFleetsNanoforgePower(fleet)))/*+1*/;
+        }catch (Exception e){
+            AIRetrofit_Log.loging("failed to get the forge power in fleet. error: "+e,new AIRetrofits_RobotForgeSecondary().getClass(),true);
+            return 0f;
         }
+    }
 
 
     public static float getFleetsForgeModules(CampaignFleetAPI fleet){
